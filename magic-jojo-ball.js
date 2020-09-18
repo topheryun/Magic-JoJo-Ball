ed7ae6e14dd300359032abff711b4eba681cd0f4
topheryun <46732742+topheryun@users.noreply.github.com>
2020-09-18 14:55:17 -0700

    updated the ask button to show more memes
    created getImageSourceString() to generate a random meme with a 2-1-1 ratio

--- magic-jojo-ball.js
+++ magic-jojo-ball.js
@@ -1,14 +1,11 @@
 
-/* ==================================================
-Loads functions after HTML is done loading.
-================================================== */
 window.onload=function() {
 	document.getElementById('ask-button-id').addEventListener('click', loadMemeAnswer);
-	
 }
 
 /* ==================================================
-Array of gifs. Might not need this.
+Array of gifs. Might not need this. Marc says it might
+have a use later
 ================================================== */
 var memeAnswers = [
 	"yes01.gif",
@@ -21,22 +18,41 @@ This function activates when the "Ask" button is pressed.
 It chooses a random meme answer and displays it.
 ================================================== */
 function loadMemeAnswer() {
-	
-	var randomNumber = Math.floor(Math.random() * 3) + 1;
-	var img = new Image();
-	img.src = "yes0" + randomNumber + ".gif";
 	/*
+	var img = new Image();
+	img.src = getImageSourceString();
+	
 	if (!meme-opened) {
 		document.getElementById('meme-area').style.display = 'block';
 		// document.getElementById('meme-area').style.opacity = 1;
 	}
 	*/
 
-	document.getElementById("testgif").src = img.src;
-
+	document.getElementById("meme-area-id").src = getImageSourceString();
+	// console.log("meme-area-id");
 }
 
-
+/* ==================================================
+Retrieve yes/no/maybe gif string with a 2/1/1 ratio
+================================================== */
+function getImageSourceString() {
+	// 0-1: yes, 2: no, 3: maybe
+	var answerType = Math.floor(Math.random() * 4);
+	
+	if (answerType <= 1) { // yes
+		var randomNumber = Math.floor(Math.random() * 3) + 1;
+		return "yes0" + randomNumber + ".gif";
+	}
+	else if (answerType == 2) { // no
+		var randomNumber = Math.floor(Math.random() * 3) + 1;
+		return "no0" + randomNumber + ".gif";
+	}
+	else if (answerType == 3) { // maybe
+		var randomNumber = Math.floor(Math.random() * 4) + 1;
+		return "maybe0" + randomNumber + ".gif";
+	}
+	
+}
 
 
 
