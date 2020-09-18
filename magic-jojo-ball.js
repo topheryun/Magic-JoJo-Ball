be5b1c6696e78c96b5cc6636b359178741a5176f
topheryun <46732742+topheryun@users.noreply.github.com>
2020-09-18 12:38:46 -0700

    the js file!
    doesn't work with current html, but should work with some changes to variable names. fingers crossed.

---
+++ magic-jojo-ball.js
@@ -0,0 +1,45 @@
+
+/* ==================================================
+Loads functions after HTML is done loading.
+================================================== */
+window.onload=function() {
+	document.getElementById('ask-button-id').addEventListener('click', loadMemeAnswer);
+	
+}
+
+/* ==================================================
+Array of gifs. Might not need this.
+================================================== */
+var memeAnswers = [
+	"yes01.gif",
+	"yes02.gif",
+	"yes03.gif"
+];
+
+/* ==================================================
+This function activates when the "Ask" button is pressed.
+It chooses a random meme answer and displays it.
+================================================== */
+function loadMemeAnswer() {
+	
+	var randomNumber = Math.floor(Math.random() * 3) + 1;
+	var img = new Image();
+	img.src = "yes0" + randomNumber + ".gif";
+	/*
+	if (!meme-opened) {
+		document.getElementById('meme-area').style.display = 'block';
+		// document.getElementById('meme-area').style.opacity = 1;
+	}
+	*/
+
+	document.getElementById("testgif").src = img.src;
+
+}
+
+
+
+
+
+/* ==================================================
+
+================================================== */
